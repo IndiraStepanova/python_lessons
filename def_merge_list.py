a7c384ae761398af5bc89d31873aa9cac4dc67f8
Indira Stepanova <m_indira_i@hotmail.com>
2022-11-30 23:54:02 +0300

    quick_merge

---
+++ def_merge_list.py
@@ -0,0 +1,36 @@
+def quick_merge(list1, list2):
+    result = []
+
+    p1 = 0  # указатель на первый элемент списка list1
+    p2 = 0  # указатель на первый элемент списка list2
+
+    while p1 < len(list1) and p2 < len(list2):  # пока не закончился хотя бы один список
+        if list1[p1] <= list2[p2]:
+            result.append(list1[p1])
+            p1 += 1
+        else:
+            result.append(list2[p2])
+            p2 += 1
+
+    if p1 < len(list1):  # прицепление остатка
+        result += list1[p1:]
+    if p2 < len(list2):
+        result += list2[p2:]
+
+    return result
+
+
+# считываем данные
+strings_cnt = int(input())
+sorted_list = []
+for _ in range(strings_cnt):
+    strings = [int(i) for i in input().split()]
+
+    # вызываем функцию
+    sorted_list = quick_merge(strings, sorted_list)
+print(*sorted_list)
+
+
+
+
+
