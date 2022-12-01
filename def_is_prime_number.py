f454d6146a6ecc35624b6cfc7eb63dd44d3c157a
Indira Stepanova <m_indira_i@hotmail.com>
2022-12-01 19:38:39 +0300

    is_prime_number

---
+++ def_is_prime_number.py
@@ -0,0 +1,16 @@
+def is_prime_number(number):
+    cnt = 0
+    if number == 1:
+        return False
+    else:
+        for factor in range(2, number):
+            if number % factor == 0:
+                return False
+        return True
+
+
+# считываем данные
+num = int(input())
+
+# вызываем функцию
+print(is_prime_number(num))
